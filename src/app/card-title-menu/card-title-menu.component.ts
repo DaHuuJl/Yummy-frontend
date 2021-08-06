275e6576798f59fc63d63e6b4ab10f39cf29f6ce
venar <venart-2@yandex.ru>
2021-08-06 14:59:30 +0300

    Add card-menu and card-title-menu

---
+++ src/app/card-title-menu/card-title-menu.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card-title-menu',
+  templateUrl: './card-title-menu.component.html',
+  styleUrls: ['./card-title-menu.component.css']
+})
+export class CardTitleMenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
